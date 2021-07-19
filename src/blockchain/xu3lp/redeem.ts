f415aa4083b634c2bcf22661099e06ed89b3c990
Eswara Sai <eswarasai.v@gmail.com>
2021-07-19 19:26:17 +0530

    Removed slippage from max redeem qty for xU3LP

--- src/blockchain/xu3lp/redeem.ts
+++ src/blockchain/xu3lp/redeem.ts
@@ -1,5 +1,4 @@
 import { BaseProvider } from '@ethersproject/providers'
-import { BigNumberish } from 'ethers'
 import { formatEther } from 'ethers/lib/utils'
 
 import { ILPTokenSymbols, IU3LPAssetId } from '../../types/xToken'
@@ -17,24 +16,7 @@ export const getMaximumRedeemableXU3LP = async (
     ? xu3lpContract.getBufferToken0Balance
     : xu3lpContract.getBufferToken1Balance
 
-  const [bufferHoldings, nav, totalSupply] = await Promise.all([
-    getBufferBalance(),
-    xu3lpContract.getNav(),
-    xu3lpContract.totalSupply(),
-  ])
+  const bufferHoldings = await getBufferBalance()
 
-  const getAmountInAssetTerms = outputAsset
-    ? xu3lpContract.getAmountInAsset0Terms
-    : xu3lpContract.getAmountInAsset1Terms
-
-  const amount = bufferHoldings
-    .mul(totalSupply as BigNumberish)
-    .div(nav as BigNumberish)
-
-  const redeemable = await getAmountInAssetTerms(amount)
-
-  // Account for slippage buffer of 1%
-  const redeemableWithSlippage = redeemable.sub(redeemable.div(100))
-
-  return formatEther(redeemableWithSlippage)
+  return formatEther(bufferHoldings)
 }
