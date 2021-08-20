a370255569f060a597211dac79e283f45f3c166d
Robb Veltman <robb.veltman@gmail.com>
2021-08-20 11:01:24 -0400

    Use new infura API key

--- src/constants.spec.ts
+++ src/constants.spec.ts
@@ -7,7 +7,7 @@ export const testAddress = process.env.TEST_ADDRESS as string
 
 export const provider = new ethers.providers.InfuraProvider(
   'homestead',
-  '01a0666704244d44812fc70d214a913b'
+  '20a1be3a49a94ad09b18d51d3a25ad84'
 )
 
 export const kovanProvider = new ethers.providers.InfuraProvider(
