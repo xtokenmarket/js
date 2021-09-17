0f88086fbed2a8d69f4a39135ac7a6467cd73ea1
Rory Murray <21082125+rorychristianmurray@users.noreply.github.com>
2021-09-17 19:10:51 -0400

    Adds getXAlphaAsset

--- src/blockchain/xalpha/assets.ts
+++ src/blockchain/xalpha/assets.ts
@@ -4,5 +4,27 @@ import { X_ALPHA_A } from '@xtoken/abis'
 import { IAsset } from '../../types/xToken'
 
 import { getXAlphaContracts } from './helper'
+import { getXAlphaPrices } from './prices'
 
-// get the xalpha abi
+export const getXAlphaAsset = async (
+  symbol: typeof X_ALPHA_A,
+  provider: BaseProvider
+): Promise<IAsset> => {
+  const { kyberProxyContract, xalphaContract } = await getXAlphaContracts(
+    symbol,
+    provider
+  )
+
+  const { aum, priceEth, priceUsd } = await getXAlphaPrices(
+    xalphaContract,
+    kyberProxyContract
+  )
+
+  return {
+    aum,
+    mandate: 'Dynamic Allocator; Buchanan Mandate', // check this
+    price: priceUsd,
+    priceEth,
+    symbol,
+  }
+}
