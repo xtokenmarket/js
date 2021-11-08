b094a0b6bf9c173594f23c014bcba0cbdbf9c885
Robb Veltman <robb.veltman@gmail.com>
2021-11-08 13:02:19 -0500

    Add arbitrum test provider

--- src/blockchain/exchanges/uniswapV3.spec.ts
+++ src/blockchain/exchanges/uniswapV3.spec.ts
@@ -1,7 +1,7 @@
 import { BUY, SELL, X_AAVE_A } from '@xtoken/abis'
 import test from 'ava'
 
-import { provider } from '../../constants.spec'
+import { arbitrumProvider, provider } from '../../constants.spec'
 
 import { getEthUsdcPriceUniswapV3, getUniswapV3EstimatedQty } from './uniswapV3'
 
@@ -32,7 +32,14 @@ test('Calculate expected quantity on burn of xAAVEa on UniswapV3', async (t) => 
 })
 
 test('Get ETH price in USDC on UniswapV3', async (t) => {
-  const expectedQty = await getEthUsdcPriceUniswapV3(provider)
-  console.log('[UniswapV3] 1 ETH price in USDC:', expectedQty)
-  t.true(Number(expectedQty) > 0)
+  const expectedQtyMainnet = await getEthUsdcPriceUniswapV3(provider)
+  console.log('[UniswapV3] 1 ETH price in USDC - Mainnet:', expectedQtyMainnet)
+  t.true(Number(expectedQtyMainnet) > 0)
+
+  const expectedQtyArbitrum = await getEthUsdcPriceUniswapV3(arbitrumProvider)
+  console.log(
+    '[UniswapV3] 1 ETH price in USDC - Arbitrum:',
+    expectedQtyArbitrum
+  )
+  t.true(Number(expectedQtyArbitrum) > 0)
 })
--- src/constants.spec.ts
+++ src/constants.spec.ts
@@ -15,3 +15,9 @@ export const ropstenProvider = new ethers.providers.InfuraProvider(
   'ropsten',
   infuraApiKey
 )
+
+export const arbitrumProvider = new ethers.providers.JsonRpcProvider(
+  // 'https://arb1.arbitrum.io/rpc'
+  // 'https://arbitrum-mainnet.infura.io/v3/70c60a289bcf412ba4ac856777ce3ef1'
+  'https://arb-mainnet.g.alchemy.com/v2/asY2H6QpivyJ_yM9bZQfFFW8JjFoREGA'
+)
